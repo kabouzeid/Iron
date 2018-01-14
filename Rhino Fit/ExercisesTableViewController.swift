cf08399ac5ae387af66afb31e7852db3eb9ecea6
Karim Abou Zeid <karim23697@gmail.com>
2018-01-14 23:29:31 +0100

    Basic layout

---
+++ Rhino Fit/ExercisesTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ExercisesTableViewController.swift
+//  Rhino Fit
+//
+//  Created by Karim Abou Zeid on 14.01.18.
+//  Copyright © 2018 Karim Abou Zeid Software. All rights reserved.
+//
+
+import UIKit
+
+class ExercisesTableViewController: UITableViewController {
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "exercise", for: indexPath)
+        return cell
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+
+}
