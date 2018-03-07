acaf32809c1a8db898b51100eefa414de9fc2f87
Karim Abou Zeid <karim23697@gmail.com>
2018-03-07 20:23:43 +0100

    Show trainings on profile page

--- Rhino Fit/Extension/TimeIntervalExtension.swift
+++ Rhino Fit/Extension/TimeIntervalExtension.swift
@@ -10,11 +10,24 @@ import Foundation
 
 extension TimeInterval {
     func stringFormatted() -> String {
-        
         let hours = Int(self) / 3600
         let minutes = Int(self / 60) - Int(hours * 60)
         let seconds = Int(self) - (Int(self / 60) * 60)
         
         return String(format: "%0.2d:%0.2d:%0.2d", hours, minutes, seconds)
+    }
+    
+    func stringFormattedWithLetters() -> String {
+        let hours = Int(self) / 3600
+        let minutes = Int(self / 60) - Int(hours * 60)
+        let seconds = Int(self) - (Int(self / 60) * 60)
+        
+        if hours == 0 {
+            if minutes == 0 {
+                return String(format: "%ds", seconds)
+            }
+            return String(format: "%dm %ds", minutes, seconds)
+        }
+        return String(format: "%dh %dm", hours, minutes + (seconds >= 30 ? 1 : 0)) // round seconds
     }
 }
---
+++ Rhino Fit/UI/Profile/TrainingsTableViewController.swift
@@ -0,0 +1,80 @@
+//
+//  RecentTrainingsTableViewController.swift
+//  Rhino Fit
+//
+//  Created by Karim Abou Zeid on 03.03.18.
+//  Copyright © 2018 Karim Abou Zeid Software. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TrainingsTableViewController: UITableViewController {
+    
+    private var trainings: [Training]?
+
+    private let dateFormatter = DateFormatter()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .short
+        dateFormatter.doesRelativeDateFormatting = true
+        
+         self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let request: NSFetchRequest<Training> =  Training.fetchRequest()
+        request.predicate = NSPredicate(format: "isCurrentTraining != %@", NSNumber(booleanLiteral: true))
+        request.sortDescriptors = [NSSortDescriptor(key: "start", ascending: false)]
+        trainings = try? AppDelegate.instance.persistentContainer.viewContext.fetch(request)
+        tableView.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1 // TODO section for this week, this month etc.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return trainings?.count ?? 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "training cell", for: indexPath)
+        let training = trainings![indexPath.row]
+        
+        cell.textLabel?.text = training.displayTitle
+        cell.detailTextLabel?.text = "\(dateFormatter.string(from: training.start!)) | \(training.end!.timeIntervalSince(training.start!).stringFormattedWithLetters())"
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            AppDelegate.instance.persistentContainer.viewContext.delete(trainings![indexPath.row])
+            trainings!.remove(at: indexPath.row)
+            AppDelegate.instance.saveContext()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
