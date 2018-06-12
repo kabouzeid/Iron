2eb6b9d25c7f00754310caa2175e30d6b5fb5cf3
Karim Abou Zeid <karim23697@gmail.com>
2018-06-13 00:33:48 +0200

    No longer use NSFetchedResultsController

--- Rhino Fit/UI/Profile/TrainingsTableViewController.swift
+++ Rhino Fit/UI/Profile/TrainingsTableViewController.swift
@@ -10,52 +10,57 @@ import UIKit
 import CoreData
 
 class TrainingsTableViewController: UITableViewController {
-    
-    private var fetchedResultsController: NSFetchedResultsController<Training>!
-    private var sevenDaysFetchedResultsController: NSFetchedResultsController<Training>!
-    private var fourteenDaysFetchedResultsController: NSFetchedResultsController<Training>!
 
-    private let summaryFetchedResultsDelegate = SimpleFetchedResultsControllerDelegate()
+    private var trainings: [Training]?
+
+    private var fetchRequest: NSFetchRequest<Training> {
+        let request: NSFetchRequest<Training> =  Training.fetchRequest()
+        request.predicate = NSPredicate(format: "isCurrentTraining != %@", NSNumber(booleanLiteral: true))
+        request.sortDescriptors = [NSSortDescriptor(key: "start", ascending: false)]
+        return request
+    }
 
     @IBOutlet weak var summaryView: SummaryView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.navigationItem.rightBarButtonItem = self.editButtonItem
-
-        initFetchedResultsController()
-        initSummaryFetchedResultsControllers()
-        try? fetchedResultsController.performFetch()
-        try? sevenDaysFetchedResultsController.performFetch()
-        try? fourteenDaysFetchedResultsController.performFetch()
-
-        updateSummary()
     }
 
-    private func initFetchedResultsController() {
-        let request: NSFetchRequest<Training> =  Training.fetchRequest()
-        request.predicate = NSPredicate(format: "isCurrentTraining != %@", NSNumber(booleanLiteral: true))
-        request.sortDescriptors = [NSSortDescriptor(key: "start", ascending: false)]
-        fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: AppDelegate.instance.persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
-        fetchedResultsController.delegate = self
+    override func viewWillAppear(_ animated: Bool) {
+        var fullReload = true
+        if let selection = tableView.indexPathForSelectedRow {
+            let fetchedTrainings = try? AppDelegate.instance.persistentContainer.viewContext.fetch(fetchRequest)
+            if trainings!.first!.objectID == fetchedTrainings?.first?.objectID { // the training did not move positions
+                fullReload = false
+                tableView.reloadRows(at: [selection], with: .none)
+                tableView.selectRow(at: selection, animated: false, scrollPosition: .none)
+            }
+        }
+
+        super.viewWillAppear(animated)
+
+        if fullReload { // we don't know where a new training might have been inserted, reload everything!
+            trainings = try? AppDelegate.instance.persistentContainer.viewContext.fetch(fetchRequest)
+            tableView.reloadData()
+        }
+        updateSummary()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // TODO: section for this week, this month etc.
-        return fetchedResultsController.sections?.count ?? 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let sections = fetchedResultsController.sections!
-        return sections[section].numberOfObjects
+        return trainings?.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "training cell", for: indexPath)
-        let training = fetchedResultsController.object(at: indexPath)
+        let training = trainings![indexPath.row]
 
         cell.textLabel?.text = training.displayTitle
         cell.detailTextLabel?.text = "\(Training.dateFormatter.string(from: training.start!)) for \(Training.durationFormatter.string(from: training.duration)!)"
@@ -65,8 +70,12 @@ class TrainingsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            fetchedResultsController.managedObjectContext.delete(fetchedResultsController.object(at: indexPath))
-            AppDelegate.instance.saveContext()
+            let training = trainings![indexPath.row]
+            trainings?.remove(at: indexPath.row)
+            AppDelegate.instance.persistentContainer.viewContext.delete(training)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+
+            updateSummary()
         }
     }
 
@@ -75,42 +84,33 @@ class TrainingsTableViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let trainingDetailTableViewController = segue.destination as? TrainingDetailTableViewController,
             let indexPath = tableView.indexPathForSelectedRow {
-            trainingDetailTableViewController.training = fetchedResultsController.object(at: indexPath)
+            trainingDetailTableViewController.training = trainings![indexPath.row]
             trainingDetailTableViewController.isEditable = true
         }
     }
 
     // MARK: - Summary View
 
-    private func initSummaryFetchedResultsControllers() {
-        summaryFetchedResultsDelegate.onDidChange = updateSummary
-
-        let calendar = Calendar(identifier: .iso8601)
-
-        let sevenDaysAgo = calendar.date(byAdding: .day, value: -7, to: Date())!
+    private func updateSummary() {
+        // create the fetch requests
+        let sevenDaysAgo = Calendar.current.date(byAdding: .day, value: -7, to: Date())!
         let sevenDaysRequest: NSFetchRequest<Training> = Training.fetchRequest()
         sevenDaysRequest.predicate = NSPredicate(format: "isCurrentTraining != %@ AND start >= %@",
                                                  NSNumber(booleanLiteral: true),
                                                  sevenDaysAgo as NSDate)
-        sevenDaysRequest.sortDescriptors = [NSSortDescriptor(key: "start", ascending: false)]
-        sevenDaysFetchedResultsController = NSFetchedResultsController(fetchRequest: sevenDaysRequest, managedObjectContext: AppDelegate.instance.persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
-        sevenDaysFetchedResultsController.delegate = summaryFetchedResultsDelegate
 
-        let fourteenDaysAgo = calendar.date(byAdding: .day, value: -7, to: sevenDaysAgo)!
+        let fourteenDaysAgo = Calendar.current.date(byAdding: .day, value: -7, to: sevenDaysAgo)!
         let fourteenDaysRequest: NSFetchRequest<Training> = Training.fetchRequest()
         fourteenDaysRequest.predicate = NSPredicate(format: "isCurrentTraining != %@ AND start >= %@ AND start < %@",
                                                     NSNumber(booleanLiteral: true),
                                                     fourteenDaysAgo as NSDate,
                                                     sevenDaysAgo as NSDate)
-        fourteenDaysRequest.sortDescriptors = [NSSortDescriptor(key: "start", ascending: false)]
-        fourteenDaysFetchedResultsController = NSFetchedResultsController(fetchRequest: fourteenDaysRequest, managedObjectContext: AppDelegate.instance.persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
-        fourteenDaysFetchedResultsController.delegate = summaryFetchedResultsDelegate
-    }
 
-    private func updateSummary() {
-        let trainingsFromSevenDaysAgo = sevenDaysFetchedResultsController.fetchedObjects ?? []
-        let trainingsFromFourteenDaysAgo = fourteenDaysFetchedResultsController.fetchedObjects ?? []
+        // fetch the objects
+        let trainingsFromSevenDaysAgo = (try? AppDelegate.instance.persistentContainer.viewContext.fetch(sevenDaysRequest)) ?? []
+        let trainingsFromFourteenDaysAgo = (try? AppDelegate.instance.persistentContainer.viewContext.fetch(fourteenDaysRequest)) ?? []
 
+        // compute the values
         let valuesSevenDaysAgo = trainingsFromSevenDaysAgo.reduce((0, 0, 0)) { (result, training) -> (TimeInterval, Int, Float) in
             return (result.0 + training.duration, result.1 + training.numberOfCompletedSets, result.2 + training.totalCompletedWeight)
         }
@@ -118,6 +118,7 @@ class TrainingsTableViewController: UITableViewController {
             return (result.0 + training.duration, result.1 + training.numberOfCompletedSets, result.2 + training.totalCompletedWeight)
         }
 
+        // set the values
         let durationEntry = summaryView.entries[0]
         let setsEntry = summaryView.entries[1]
         let weightEntry = summaryView.entries[2]
@@ -174,41 +175,5 @@ class TrainingsTableViewController: UITableViewController {
         }
         weightEntry.detail.text! += String(format: "%.1f", weightPercent) + "%"
         weightEntry.detail.isHidden = false
-    }
-}
-
-// MARK: - Fetched results controller delegate
-
-extension TrainingsTableViewController : NSFetchedResultsControllerDelegate {
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.beginUpdates()
-    }
-
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-        switch type {
-        case .insert:
-            tableView.insertSections([sectionIndex], with: .automatic)
-        case .delete:
-            tableView.deleteSections([sectionIndex], with: .automatic)
-        default:
-            break
-        }
-    }
-
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        switch type {
-        case .insert:
-            tableView.insertRows(at: [newIndexPath!], with: .automatic)
-        case .delete:
-            tableView.deleteRows(at: [indexPath!], with: .automatic)
-        case .update:
-            tableView.reloadRows(at: [indexPath!], with: .automatic)
-        case .move:
-            tableView.moveRow(at: indexPath!, to: newIndexPath!)
-        }
-    }
-
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.endUpdates()
     }
 }
