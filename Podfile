a7e255d86eea44ba75962e734501af45d983e154
Karim Abou Zeid <karim23697@gmail.com>
2018-09-08 15:30:53 +0200

    Add Charts pod

--- Podfile
+++ Podfile
@@ -6,6 +6,8 @@ target 'Rhino Fit' do
   # Pods for Rhino Fit
   pod 'SwiftyJSON'
   pod 'Benchmark'
+  # Only until Swift 4.2 is officially supported by Charts
+  pod 'Charts', :git => 'https://github.com/jlcanale/Charts', :branch => 'Swift-4.2', :inhibit_warnings => true
 
   target 'Rhino FitTests' do
     inherit! :search_paths
--- Pods/Target Support Files/Pods-Rhino Fit/Pods-Rhino Fit-frameworks.sh
+++ Pods/Target Support Files/Pods-Rhino Fit/Pods-Rhino Fit-frameworks.sh
@@ -144,10 +144,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Benchmark/Benchmark.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Benchmark/Benchmark.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
