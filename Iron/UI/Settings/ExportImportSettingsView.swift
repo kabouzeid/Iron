1171ff83b033a0ac2058c5f4c677c225a853bbad
Karim Abou Zeid <karim23697@gmail.com>
2019-09-17 14:06:24 +0200

    Move export/import to own view

---
+++ Iron/UI/Settings/ExportImportSettingsView.swift
@@ -0,0 +1,90 @@
+//
+//  ExportImportSettingsView.swift
+//  Iron
+//
+//  Created by Karim Abou Zeid on 17.09.19.
+//  Copyright © 2019 Karim Abou Zeid Software. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+
+struct ExportImportSettingsView: View {
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @EnvironmentObject var settingsStore: SettingsStore
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Export Workout Data".uppercased())) {
+                Button("As JSON File") {
+                    guard let trainings = self.fetchTrainings() else { return }
+                    
+                    let encoder = JSONEncoder()
+                    encoder.outputFormatting = [.prettyPrinted, .withoutEscapingSlashes]
+                    encoder.dateEncodingStrategy = .iso8601
+                    
+                    guard let data = try? encoder.encode(trainings) else { return }
+                    guard let url = self.writeFile(data: data, name: "workout_data.json") else { return }
+                    self.shareFile(url: url)
+                }
+//                Button("As XML File") {
+//                    // TODO
+//                }
+                Button("As Plain Text File") {
+                    guard let trainings = self.fetchTrainings() else { return }
+                    
+                    let text = trainings.compactMap { $0.logText(weightUnit: self.settingsStore.weightUnit) }.joined(separator: "\n\n\n\n\n")
+                    
+                    guard let data = text.data(using: .utf8) else { return }
+                    guard let url = self.writeFile(data: data, name: "workout_data.txt") else { return }
+                    self.shareFile(url: url)
+                }
+            }
+            
+            Section(header: Text("Import Workout Data".uppercased())) {
+                Button("From JSON File") {
+                    // TODO
+                }.disabled(true)
+//                Button("From XML File") {
+//                    // TODO
+//                }
+            }
+        }
+    }
+    
+    private func fetchTrainings() -> [Training]? {
+        let request: NSFetchRequest<Training> = Training.fetchRequest()
+        request.predicate = NSPredicate(format: "\(#keyPath(Training.isCurrentTraining)) != %@", NSNumber(booleanLiteral: true))
+        request.sortDescriptors = [NSSortDescriptor(keyPath: \Training.start, ascending: false)]
+        return (try? self.managedObjectContext.fetch(request))
+    }
+    
+    private func writeFile(data: Data, name: String) -> URL? {
+        guard let path = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else { return nil }
+        let url = path.appendingPathComponent(name)
+        do {
+            try data.write(to: url, options: .atomic)
+            return url
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    private func shareFile(url: URL) {
+        let ac = UIActivityViewController(activityItems: [url], applicationActivities: nil)
+        // TODO: replace this hack with a proper way to retreive the rootViewController
+        guard let rootVC = (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.window?.rootViewController else { return }
+        rootVC.present(ac, animated: true)
+    }
+}
+
+#if DEBUG
+struct ExportImportSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExportImportSettingsView()
+            .environmentObject(mockSettingsStoreMetric)
+            .environment(\.managedObjectContext, mockManagedObjectContext)
+    }
+}
+#endif
--- Iron/UI/Settings/SettingsView.swift
+++ Iron/UI/Settings/SettingsView.swift
@@ -7,12 +7,8 @@
 //
 
 import SwiftUI
-import Combine
-import CoreData
-import UIKit
 
 struct SettingsView : View {
-    @Environment(\.managedObjectContext) var managedObjectContext
     @EnvironmentObject var settingsStore: SettingsStore
 
     var body: some View {
@@ -46,59 +42,13 @@ struct SettingsView : View {
                 }
                 
                 Section {
-                    Button("Export workout data as JSON") {
-                        guard let trainings = self.fetchTrainings() else { return }
-                        
-                        let encoder = JSONEncoder()
-                        encoder.outputFormatting = [.prettyPrinted, .withoutEscapingSlashes]
-                        encoder.dateEncodingStrategy = .iso8601
-                        
-                        guard let data = try? encoder.encode(trainings) else { return }
-                        guard let url = self.writeFile(data: data, name: "workout_data.json") else { return }
-                        self.shareFile(url: url)
+                    NavigationLink(destination: ExportImportSettingsView()) {
+                        Text("Export / Import Workout Data")
                     }
-                    Button("Export workout data as Plain Text") {
-                        guard let trainings = self.fetchTrainings() else { return }
-                        
-                        let text = trainings.compactMap { $0.logText(weightUnit: self.settingsStore.weightUnit) }.joined(separator: "\n\n\n\n\n")
-
-                        guard let data = text.data(using: .utf8) else { return }
-                        guard let url = self.writeFile(data: data, name: "workout_data.txt") else { return }
-                        self.shareFile(url: url)
-                    }
-//                    Button("Import workout data from JSON") {
-//                        // TODO
-//                    }
                 }
             }
             .navigationBarTitle(Text("Settings"))
         }
-    }
-    
-    private func fetchTrainings() -> [Training]? {
-        let request: NSFetchRequest<Training> = Training.fetchRequest()
-        request.predicate = NSPredicate(format: "\(#keyPath(Training.isCurrentTraining)) != %@", NSNumber(booleanLiteral: true))
-        request.sortDescriptors = [NSSortDescriptor(keyPath: \Training.start, ascending: false)]
-        return (try? self.managedObjectContext.fetch(request))
-    }
-    
-    private func writeFile(data: Data, name: String) -> URL? {
-        guard let path = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else { return nil }
-        let url = path.appendingPathComponent(name)
-        do {
-            try data.write(to: url, options: .atomic)
-            return url
-        } catch {
-            print(error)
-            return nil
-        }
-    }
-    
-    private func shareFile(url: URL) {
-        let ac = UIActivityViewController(activityItems: [url], applicationActivities: nil)
-        // TODO: replace this hack with a proper way to retreive the rootViewController
-        guard let rootVC = (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.window?.rootViewController else { return }
-        rootVC.present(ac, animated: true)
     }
 }
 
