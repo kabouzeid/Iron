9a12960272af9056701518e20dc7286249e28654
Karim Abou Zeid <karim23697@gmail.com>
2019-10-06 00:05:39 +0200

    Rename TrainingsData to WorkoutData

--- Iron/AppDelegate.swift
+++ Iron/AppDelegate.swift
@@ -69,7 +69,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "TrainingsData")
+        let container = NSPersistentContainer(name: "WorkoutData")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
