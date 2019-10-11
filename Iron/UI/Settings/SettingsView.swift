fe6f5f87b1990ef48c20621c34edac7dd9baa848
Karim Abou Zeid <karim23697@gmail.com>
2019-10-11 15:07:49 +0200

    Rate button goes to appstore

--- Iron/UI/Settings/SettingsView.swift
+++ Iron/UI/Settings/SettingsView.swift
@@ -73,13 +73,8 @@ struct SettingsView : View {
     private var ratingSection: some View {
         Section(footer: Text("If you like Iron, consider leaving a review, it helps a lot!")) {
             Button(action: {
-                SKStoreReviewController.requestReview()
-                
-                // TODO: replace with the below once app is in app store
-                // Note: Replace the XXXXXXXXXX below with the App Store ID for your app
-                //       You can find the App Store ID in your app's product URL
-//                guard let writeReviewURL = URL(string: "https://itunes.apple.com/app/idXXXXXXXXXX?action=write-review") else { return }
-//                UIApplication.shared.open(writeReviewURL)
+                guard let writeReviewURL = URL(string: "https://itunes.apple.com/app/id1479893244?action=write-review") else { return }
+                UIApplication.shared.open(writeReviewURL)
             }) {
                 HStack {
                     Text("Rate Iron")
