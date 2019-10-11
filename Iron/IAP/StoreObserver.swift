1ed6593869e8fdfeda21f51fdebe6cd65913c804
Karim Abou Zeid <karim23697@gmail.com>
2019-10-11 12:45:50 +0200

    restoring debug log

--- Iron/IAP/StoreObserver.swift
+++ Iron/IAP/StoreObserver.swift
@@ -24,6 +24,7 @@ class StoreObserver: NSObject {
     }
     
     func restore() {
+        print("restoring transactions...")
         SKPaymentQueue.default().restoreCompletedTransactions()
     }
 }
