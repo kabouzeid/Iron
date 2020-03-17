885c70fbc1ca905c4c8157b8a52e75c7ad2a8dec
Karim Abou Zeid <karim23697@gmail.com>
2020-03-17 18:14:10 +0100

    DeveloperSettings

---
+++ Iron/_debug/DeveloperSettings.swift
@@ -0,0 +1,88 @@
+//
+//  DeveloperSettings.swift
+//  Iron
+//
+//  Created by Karim Abou Zeid on 17.03.20.
+//  Copyright © 2020 Karim Abou Zeid Software. All rights reserved.
+//
+
+#if DEBUG
+
+import SwiftUI
+import WorkoutDataKit
+
+struct DeveloperSettings: View {
+    var body: some View {
+        Button("Create default workout plans") {
+            createDefaultWorkoutPlans()
+        }.navigationBarTitle("Developer", displayMode: .inline)
+    }
+}
+
+func toUuid(_ id: Int) -> UUID? {
+    ExerciseStore.shared.exercises.first { $0.everkineticId == id }?.uuid
+}
+
+func niceWeight(weight: Double, unit: WeightUnit) -> Double {
+    let weightInUnit = WeightUnit.convert(weight: weight, from: .metric, to: unit)
+    let nice = weightInUnit - weightInUnit.truncatingRemainder(dividingBy: unit.barbellIncrement)
+    return WeightUnit.convert(weight: nice, from: unit, to: .metric)
+}
+
+func createDefaultWorkoutPlans() {
+    let context = WorkoutDataStorage.shared.persistentContainer.viewContext
+    let unit = SettingsStore.shared.weightUnit
+
+    let create5x5 = { (weight: Double) -> [WorkoutRoutineSet] in
+        (0..<5).map { _ -> WorkoutRoutineSet in
+            let set = WorkoutRoutineSet(context: context)
+            set.repetitionsMinValue = 8
+            set.repetitionsMaxValue = 12
+            return set
+        }
+    }
+
+    let workoutRoutineExerciseSquatA = WorkoutRoutineExercise(context: context)
+    workoutRoutineExerciseSquatA.exerciseUuid = toUuid(122) // squat
+    workoutRoutineExerciseSquatA.workoutRoutineSets = NSOrderedSet(array: create5x5(niceWeight(weight: 120, unit: unit)))
+
+    let workoutRoutineExerciseBenchA = WorkoutRoutineExercise(context: context)
+    workoutRoutineExerciseBenchA.exerciseUuid = toUuid(42) // bench
+    workoutRoutineExerciseBenchA.workoutRoutineSets = NSOrderedSet(array: create5x5(niceWeight(weight: 80, unit: unit)))
+
+    let workoutRoutineExerciseRowA = WorkoutRoutineExercise(context: context)
+    workoutRoutineExerciseRowA.exerciseUuid = toUuid(298) // row
+    workoutRoutineExerciseRowA.workoutRoutineSets = NSOrderedSet(array: create5x5(niceWeight(weight: 60, unit: unit)))
+
+    let workoutRoutineA = WorkoutRoutine(context: context)
+    workoutRoutineA.title = "Workout A"
+    workoutRoutineA.workoutRoutineExercises = NSOrderedSet(arrayLiteral: workoutRoutineExerciseSquatA, workoutRoutineExerciseBenchA, workoutRoutineExerciseRowA)
+
+    let workoutRoutineExerciseSquatB = WorkoutRoutineExercise(context: context)
+    workoutRoutineExerciseSquatB.exerciseUuid = toUuid(122) // squat
+    workoutRoutineExerciseSquatB.workoutRoutineSets = NSOrderedSet(array: create5x5(niceWeight(weight: 120, unit: unit)))
+
+    let workoutRoutineExerciseBenchB = WorkoutRoutineExercise(context: context)
+    workoutRoutineExerciseBenchB.exerciseUuid = toUuid(9001) // press
+    workoutRoutineExerciseBenchB.workoutRoutineSets = NSOrderedSet(array: create5x5(niceWeight(weight: 65, unit: unit)))
+
+    let workoutRoutineExerciseRowB = WorkoutRoutineExercise(context: context)
+    workoutRoutineExerciseRowB.exerciseUuid = toUuid(99) // deadlift
+    workoutRoutineExerciseRowB.workoutRoutineSets = NSOrderedSet(array: create5x5(niceWeight(weight: 140, unit: unit)))
+
+    let workoutRoutineB = WorkoutRoutine(context: context)
+    workoutRoutineB.title = "Workout B"
+    workoutRoutineB.workoutRoutineExercises = NSOrderedSet(arrayLiteral: workoutRoutineExerciseSquatB, workoutRoutineExerciseBenchB, workoutRoutineExerciseRowB)
+
+    let workoutPlan = WorkoutPlan(context: context)
+    workoutPlan.title = "StrongLifts 5x5"
+    workoutPlan.workoutRoutines = NSOrderedSet(arrayLiteral: workoutRoutineA, workoutRoutineB)
+}
+
+struct DeveloperSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        DeveloperSettings()
+    }
+}
+
+#endif
